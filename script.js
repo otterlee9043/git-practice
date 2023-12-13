a01f6dbd37fac78ac3e2eb38ef4988a5a61f9ac3
Sua Lee <otterlee99@gmail.com>
2023-12-13 14:32:39 +0900

    some-new-stuff 브랜치의 업데이트

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ const wordList = [
 // 주석 추가
 let gameLoopId = null;
 // 주석 추가
+
+// some-new-stuff의 
 const container = document.querySelector("#window");
 const score = document.querySelector("#score");
 const input = document.querySelector("#word-input");
